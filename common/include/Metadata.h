c9fda75c1e622f842d2e32296dd156f37c711f1a
Bruno-chanrio <abcermelo84@gmail.com>
2023-11-29 19:17:31 -0300

    Add files via upload

--- common/include/Metadata.h
+++ common/include/Metadata.h
@@ -5,8 +5,8 @@
 #define META_NAME			"Nintendont-MametchiKitty"
 #define META_AUTHOR			"FIX94, crediar, brunochanrio"
 
-#define META_LONG1			"Commiters: GerbilSoft, JoostinOnline, GreyRogue, Howard, Cyan \r\n\r\n Project website: https://github.com/FIX94/Nintendont "
-#define META_LONG2			"Nintendont allows you to run GameCube games on a Wii or Wii U from an SD or HDD device."
+#define META_LONG1			"Commiters: GerbilSoft, JoostinOnline, GreyRogue, Howard, Cyan, brunochanrio c Project website: https://github.com/brunochanrio/Nintendont-MametchiKitty "
+#define META_LONG2			"Nintendont allows you to run GameCube games on a Wii or Wii U from an SD or HDD device. \r\n\r\n Features: \r\n - Support 3rd Party Wiimotes, like the NEW 2in1 Motion plus or the Motion 2in1 Wiimotes \r\n - Support Xbox 360 Controllers and XInput Controller that acts as the Xbox 360 Controller"
 #define META_SHORT			"Gamecube Loader"
 
 #define META_XML			"<?xml version=\"1.0\" encoding=\"ISO-8859-1\" standalone=\"yes\"?>"
